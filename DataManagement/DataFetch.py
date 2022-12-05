6e273271db774e2c0bf200149e34c415c1c9366e
Evan Hurwitz <evan.hurwitz@avast.com>
2022-12-05 22:17:10 +0000

    removed unneeded import renamed tick for scope warning corrected return type-hint cleaned up __main__ debugging test

--- DataManagement/DataFetch.py
+++ DataManagement/DataFetch.py
@@ -1,21 +1,20 @@
 import yfinance as yf
-from datetime import datetime, timedelta
+from datetime import datetime
 import pandas as pd
 
 
 def download_data(ticker: str,
                   start_date: datetime = None,
-                  end_date: datetime = None) -> tuple((yf.ticker.Ticker, pd.DataFrame)):
+                  end_date: datetime = None) -> tuple[yf.ticker.Ticker, pd.DataFrame]:
 
-    tick = yf.Ticker(ticker)
+    download_tick = yf.Ticker(ticker)
     if start_date is None and end_date is None:
-        tick_data = tick.history(period='max')
+        tick_data = download_tick.history(period='max')
     else:
-        tick_data = tick.history(start=start_date, end=end_date)
+        tick_data = download_tick.history(start=start_date, end=end_date)
 
-    return tick, tick_data
+    return download_tick, tick_data
 
 
 if __name__ == '__main__':
-    tick, data = download_data('GOOGL')
-
+    pass
