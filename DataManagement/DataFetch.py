8966354e536250b2d34804af205d509c5e7272f3
Evan Hurwitz <evan.hurwitz@avast.com>
2022-12-05 22:43:41 +0000

    added docstring to download_data modified test to search "GOOG" instead of "GOOGL"

--- DataManagement/DataFetch.py
+++ DataManagement/DataFetch.py
@@ -5,15 +5,27 @@ import pandas as pd
 
 def download_data(ticker: str,
                   start_date: datetime = None,
-                  end_date: datetime = None) -> tuple[yf.ticker.Ticker, pd.DataFrame]:
+                  end_date: datetime = None) -> tuple[yf.ticker.Ticker, pd.DataFrame, bool]:
+    """
+    downloads the requested data from yahoo finance. The ticker is expected to be correct,
+    but will return an empty dataframe if it is incorrect.
+
+    :param ticker: text ticker of quoted stock
+    :param start_date: datetime object of start date
+    :param end_date: datetime object of end date
+    :return: ticker object, history dataframe, data found flag
+    """
 
     download_tick = yf.Ticker(ticker)
+    found = True
     if start_date is None and end_date is None:
         tick_data = download_tick.history(period='max')
     else:
         tick_data = download_tick.history(start=start_date, end=end_date)
+    if tick_data.shape[0] == 0:
+        found = False
 
-    return download_tick, tick_data
+    return download_tick, tick_data, found
 
 
 if __name__ == '__main__':
--- tests/test_DataFetch.py
+++ tests/test_DataFetch.py
@@ -2,7 +2,7 @@ from unittest import TestCase
 from datetime import datetime, timedelta
 from DataManagement.DataFetch import download_data
 
-TEST_TICKERS = ['GOOGL', 'AAPL', 'MSFT', 'ULVR.L']
+TEST_TICKERS = ['GOOG', 'AAPL', 'MSFT', 'ULVR.L']
 
 
 class DataTest(TestCase):
@@ -13,16 +13,20 @@ class DataTest(TestCase):
         each tests ticker. Runs the following tests:
         - Tests a 1-week download for each tests ticker
         - Tests a maximum history download for microsoft
+        - tests if good downloads are correctly found
+        - tests if incorrect ticker downloads return the correct errors
 
         :return: Nothing
         """
         dt_today = datetime.now()
         dt_from = dt_today - timedelta(days=7)
-        #results = {ticker: [Tick, Tick.history(start=dt_today, end=dt_from)] for ticker in TEST_TICKERS}
-        results = {}
         for ticker in TEST_TICKERS:
-            _, Tick_data = download_data(ticker, start_date=dt_from, end_date=dt_today)
+            _, Tick_data, found = download_data(ticker, start_date=dt_from, end_date=dt_today)
             self.assertGreater(Tick_data.shape[0], 0, f"no data downloaded for {ticker} for a 1-week period")
-        _, Tick_data = download_data(TEST_TICKERS[2])
+            self.assertTrue(found, f"download flag for {ticker} incorrecly flagged as {found}")
+        _, Tick_data, _ = download_data(TEST_TICKERS[2])
         self.assertGreater(Tick_data.shape[0], 0, f"maximum time download failed for {TEST_TICKERS[2]}")
+        _, _, found = download_data('NOT_A_TICKER')
+        self.assertFalse(found, f"false ticker incorrectly returned {found}")
+
 
