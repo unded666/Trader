f9d357af79a10b5e8c977e4049dad7eb0a1a70b7
Evan Hurwitz <evan.hurwitz@avast.com>
2022-12-05 14:45:11 +0000

    refactored download to use the Ticker object

--- DataManagement/DataFetch.py
+++ DataManagement/DataFetch.py
@@ -1,6 +1,17 @@
 import yfinance as yf
+from datetime import datetime
+import pandas as pd
 
+
+def download_data(ticker: str,
+                  start_date: datetime,
+                  end_date: datetime) -> tuple(yf.ticker.Ticker, pd.DataFrame):
+
+    tick = yf.Ticker(ticker)
+    tick_data = tick.history(period='max')
+
+    return tick, tick_data
 
 
 if __name__ == '__main__':
-    print('I am debugging, hear me roar!')
+    print('I am debugging code, hear me roar')
