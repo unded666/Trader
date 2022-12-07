a8075add61fd8b28ada5c6bcae7a9c032e9d64a3
Evan Hurwitz <evan.hurwitz@avast.com>
2022-12-07 23:16:43 +0000

    created unit test class for data management created unit test for saving data

---
+++ tests/test_DataManagement.py
@@ -0,0 +1,32 @@
+from unittest import TestCase
+import pandas as pd
+import os
+from DataManagement.DataManagement import (save_data,
+                                           load_data,
+                                           data_combinator)
+
+TGT_LOCATION = './test_files/'
+TGT_READ_FIL = './test_files/read_this.pkl'
+TGT_WRITE_FIL = './test_files/write_this.pkl'
+
+class DManTest(TestCase):
+
+    def setUp(self) -> None:
+
+        self.read_target = TGT_READ_FIL
+        self.write_target = TGT_WRITE_FIL
+        self.location = TGT_LOCATION
+        self.dummy_df = pd.DataFrame({'cats': [1], 'dogs': [0], 'bunnies': [72]})
+
+    def test_save_data(self):
+
+        if os.path.basename(self.write_target) in os.listdir(self.location):
+            # clear target file
+            os.remove(self.write_target)
+        self.assertFalse(os.path.basename(self.write_target) in os.listdir(self.location),
+                         f"test file not correctly deleted")
+        save_data(self.write_target, self.dummy_df)
+        files = os.listdir(self.location)
+        self.assertTrue(os.path.basename(self.write_target) in files, f"test file not correctly written")
+
+
