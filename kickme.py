77c8a072b3bc804de4bca0df8a1fc732c87e8563
Evan Hurwitz <evan.hurwitz@avast.com>
2022-12-08 09:36:52 +0000

    kickme script added to kickme branch

---
+++ kickme.py
@@ -0,0 +1,13 @@
+# import numpy as np
+
+
+def pointless_function(input_value: int) -> int:
+    """
+    this squares the input, because why not?
+
+    :param input_value: input integer
+    :return: square value output
+    """
+
+    value = input_value**2
+    return value
